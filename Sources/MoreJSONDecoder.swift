bd052d335e3df2e77314c5accaa9cc308b64b1bb
tattn (Tatsuya Tanaka) <tanakasan2525@gmail.com>
2020-04-28 23:01:50 +0900

    Clean code

--- Sources/MoreJSONDecoder.swift
+++ Sources/MoreJSONDecoder.swift
@@ -113,7 +113,7 @@ public class MoreJSONDecoder {
             let leadingUnderscoreRange = stringKey.startIndex..<firstNonUnderscore
             let trailingUnderscoreRange = stringKey.index(after: lastNonUnderscore)..<stringKey.endIndex
             
-            var components = stringKey[keyRange].split(separator: "_")
+            let components = stringKey[keyRange].split(separator: "_")
             let joinedString : String
             if components.count == 1 {
                 // No underscores in key, leave the word as is - maybe already camel cased
